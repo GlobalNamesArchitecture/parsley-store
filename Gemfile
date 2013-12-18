d5bba9616ce147d2110aa37ab4f1209a80ee824e
Dmitry Mozzherin <dmozzherin@gmail.com>
2013-12-18 09:24:38 -0500

    added coveralls support

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'biodiversity', '~> 3.1'
 gem 'redis', '~> 3.0'
 
 group :test do
-  gem 'rspec', '~> 2.13'
+  gem 'rspec', '~> 2.14'
   gem 'cucumber', '~> 1.3'
+  gem 'coveralls', '~> 0.7'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'parsley-store'
+require 'coveralls'
+Coveralls.wear!
+
+require_relative '../lib/parsley-store'
 require 'rspec'
 
 puts "You need to start Redis server on your machine"
